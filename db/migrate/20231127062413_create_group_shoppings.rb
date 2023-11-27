5176fc1f80a69583d2e7eca9bb5f20d220c75e95
Agustin Soria <soriaagustin8@gmail.com>
2023-11-27 06:40:49 -0300

    Update migrations, and schema

--- db/migrate/20231127062413_create_group_shoppings.rb
+++ db/migrate/20231127062413_create_group_shoppings.rb
@@ -2,7 +2,7 @@ class CreateGroupShoppings < ActiveRecord::Migration[7.1]
   def change
     create_table :group_shoppings do |t|
       t.references :group, null: false, foreign_key: {on_delete: :cascade}
-      t.references :shoppings, null: false, foreign_key: {on_delete: :cascade}
+      t.references :shopping, null: false, foreign_key: {on_delete: :cascade}
       t.timestamps
     end
   end
