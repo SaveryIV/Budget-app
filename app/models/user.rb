78ebb651b635cb0869a7449dfb11d42f99deb68a
Agustin Soria <soriaagustin8@gmail.com>
2023-11-27 15:49:58 -0300

    Adding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :shoppings, dependent: :destroy
     has_many :groups, dependent: :destroy
+
+    validates :name, presence: true, length: { maximum: 36 }
 end
