2f6f2fa64008882de78552098ff0ba04d0a75656
Agustin Soria <soriaagustin8@gmail.com>
2023-11-28 20:28:20 -0300

    Fixing linters

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,10 @@
 class Group < ApplicationRecord
-    has_many :group_shoppings, dependent: :destroy
-    has_many :shoppings, through: :group_shoppings, foreign_key: "shoppings_id"
-  
-    belongs_to :user
-  
-    validates :name, presence: true, length: { maximum: 36 },
-                     uniqueness: { scope: :user, message: 'User has already a group with this name' }
-    validates :icon, presence: true, length: { maximum: 250 }
+  has_many :group_shoppings, dependent: :destroy
+  has_many :shoppings, through: :group_shoppings, foreign_key: 'shoppings_id'
+
+  belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 36 },
+                   uniqueness: { scope: :user, message: 'User has already a group with this name' }
+  validates :icon, presence: true, length: { maximum: 250 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'pages/splash'
+  get 'pages/about'
+  devise_for :users
 
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'groups#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'pages#splash', as: :unauthenticated_root
+    end
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :groups, only: [:index, :new, :create, :edit, :update, :destroy] do
+    resources :shoppings
+  end
+
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
