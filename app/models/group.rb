701a48a0fc1169e2afc33cf2ccfaed51bc79e7bf
Agustin Soria <soriaagustin8@gmail.com>
2023-11-28 20:15:30 -0300

    Update Shopping model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,10 @@
 class Group < ApplicationRecord
-    belongs_to :users
-
-    validates :name, presence: true, length: { maximum: 36 }, uniqueness: { scope: :user, message: 'User has already a group with this name' }
+    has_many :group_shoppings, dependent: :destroy
+    has_many :shoppings, through: :group_shoppings, foreign_key: "shoppings_id"
+  
+    belongs_to :user
+  
+    validates :name, presence: true, length: { maximum: 36 },
+                     uniqueness: { scope: :user, message: 'User has already a group with this name' }
     validates :icon, presence: true, length: { maximum: 250 }
 end
