5519bdf9d1d3c1837296b289dbf1e302d48028e6
Agustin Soria <soriaagustin8@gmail.com>
2023-11-28 20:17:28 -0300

    Update of migrations, and New migrations

--- db/migrate/20231127062146_create_groups.rb
+++ db/migrate/20231127062146_create_groups.rb
@@ -3,8 +3,7 @@ class CreateGroups < ActiveRecord::Migration[7.1]
     create_table :groups do |t|
       t.string :name
       t.string :icon
-      t.references :user, null: false, foreign_key: { on_delete: :cascade }
-      t.references :shopping, null: false, foreign_key: {on_delete: :cascade}
+
       t.timestamps
     end
   end
